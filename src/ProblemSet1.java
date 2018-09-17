8a49d34a994b661a0829bd5507f4b45012e1cd8c
Eshita Kar <ekar@ucvts.tec.nj.us>
2018-09-17 09:26:11 -0400

    Exercise 1 through 3

--- src/ProblemSet1.java
+++ src/ProblemSet1.java
@@ -14,12 +14,32 @@ public class ProblemSet1 {
 	 */
 	
 	public static void main(String[] args) {
+		//Exercise 1
 		System.out.println("/// EXERCISE 1 /////////////////////////////////////////////////\n");
-		
+		System.out.println("   ##    ######  ######## ########");
+		System.out.println("  #  #   #     # #        #");
+		System.out.println(" #    #  #     # #        #");
+		System.out.println("######## ######  #        ########");
+		System.out.println("#      # #       #               #");
+		System.out.println("#      # #       #               #");
+		System.out.println("#      # #       ######## ########");
+		System.out.println("\n");
+		//Exercise 2
 		System.out.println("/// EXERCISE 2 /////////////////////////////////////////////////\n");
-		
+		System.out.println("  /\\     /\\     /\\");
+		System.out.println(" /  \\   /  \\   /  \\");
+		System.out.println("/    \\ /    \\ /    \\");
+		System.out.println("\\    / \\    / \\    /");
+		System.out.println(" \\  /   \\  /   \\  /");
+		System.out.println("  \\/     \\/     \\/\n");
+		//Exercise 3
 		System.out.println("/// EXERCISE 3 /////////////////////////////////////////////////\n");
-		
+		System.out.println("   \\\\\\\\\\");
+		System.out.println("  +\"\"\"\"\"+");
+		System.out.println("(|  0 0  |)");
+		System.out.println(" |   ^   |");
+		System.out.println(" |  '-'  |");
+		System.out.println("  +-----+\n");
 		System.out.println("/// EXERCISE 4 /////////////////////////////////////////////////\n");
 		
 		System.out.println("/// EXERCISE 5 /////////////////////////////////////////////////\n");
